da8992aaafa268d72aa7e408ca22d0e0d039d3e1
Aviral000 <avimalik02@gmail.com>
2024-04-26 11:13:16 +0530

    c3

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ export default function App() {
 
             <div>
                 {selectedCountry && selectedCity && selectedCity && (
-                    <div>You selected: {selectedCity}, {selectedState}, {selectedCountry}</div> )}
+                    <div>You selected {selectedCity}, {selectedState}, {selectedCountry}</div> )}
             </div>
         </div>
     </div>
